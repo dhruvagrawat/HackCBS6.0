9fcc5e382b622d86d72fd3e94a15cb66bed675fb
Tanmay692004 <tanmaybinjola0@gmail.com>
2023-11-05 04:46:14 +0530

    appointment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,34 @@
 import { useState } from 'react';
 import './App.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const [location, setLocation] = useState('');
+  const [hospital, setHospital] = useState('');
+  const [weekday, setWeekday] = useState(new Date());
+
+  const handleFormToggle = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const handleHospitalChange = (event) => {
+    setHospital(event.target.value);
+  };
+
+  const handleWeekdayChange = (date) => {
+    setWeekday(date);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic here
+  };
 
   return (
     <div>
@@ -14,11 +40,21 @@ function App() {
                 <a href="#">TinDr</a>
               </div>
               <ul className="nav-links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#">Skills</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#appointment">Appointment</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
               </ul>
             </div>
           </nav>
@@ -32,7 +68,9 @@ function App() {
               <h5 className="tagline">Aiding AIDS since yo mama's birth</h5>
               <div className="lognsign">
                 <button className="login">log in</button>
-                <a className="signup" href='#'><u>sign up</u></a>
+                <a className="signup" href="#">
+                  <u>sign up</u>
+                </a>
               </div>
             </div>
             <div className="rightsection">
@@ -49,10 +87,13 @@ function App() {
             <table border="0">
               <tr>
                 <td width="700px">
-                  <span style={{ color: '#000' }}> Welcome to <mark>TinDr</mark> </span> <br /><br />
-                  <span style={{ color: '#000', fontSize: '16px' }}> Best hopsital appointments booking site </span> <br /><br />
-                  Our platform is designed for seamless healthcare scheduling. Users can easily book appointments with top doctors in various hospitals. With a user-friendly interface, scheduling is convenient and straightforward. Our focus is on prioritizing your well-being and time efficiency. <br /><br />
-                   <br /><br />
+                  <span style={{ color: '#000' }}> Welcome to <mark>TinDr</mark> </span> <br />
+                  <br />
+                  <span style={{ color: '#000', fontSize: '16px' }}> Best hospital appointments booking site </span> <br />
+                  <br />
+                  Our platform is designed for seamless healthcare scheduling. Users can easily book appointments with top doctors in various hospitals. With a user-friendly interface, scheduling is convenient and straightforward. Our focus is on prioritizing your well-being and time efficiency. <br />
+                  <br />
+                  <br />
                   <br />
                 </td>
                 <td style={{ paddingLeft: '11em' }}> <img src='src\assets\about.jpg' width="400px" alt="about" /></td>
@@ -62,8 +103,33 @@ function App() {
         </div>
       </div>
       <div className="text">
-        <h2 id="second section">second section</h2>
-        <p>Our platform is designed for seamless healthcare scheduling. Users can easily book appointments with top doctors in various hospitals. With a user-friendly interface, scheduling is convenient and straightforward. Our focus is on prioritizing your well-being and time efficiency.</p>
+        <h2 id="appointment">Book an appointment</h2>
+        <p>Booking an appointment has never been easier</p>
+        <button onClick={handleFormToggle} className="FAQ__title">
+          Book Now <i className={`fal fa-plus ${isFormOpen ? 'fa-rotate-45' : 'fa-2x'}`}></i>
+        </button>
+        {isFormOpen && (
+          <form onSubmit={handleSubmit} className="mini-form">
+            <label>Location:</label>
+            <select value={location} onChange={handleLocationChange}>
+              <option value="Delhi">Delhi</option>
+              <option value="Haryana">Haryana</option>
+              <option value="UP">UP</option>
+            </select>
+            <br />
+            <label>Hospital Name:</label>
+            <select value={hospital} onChange={handleHospitalChange}>
+              <option value="Hospital 1">Hospital 1</option>
+              <option value="Hospital 2">Hospital 2</option>
+              <option value="Hospital 3">Hospital 3</option>
+            </select>
+            <br />
+            <label>Weekday:</label>
+            <DatePicker selected={weekday} onChange={handleWeekdayChange} dateFormat="MMMM d, yyyy" />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+        )}
       </div>
       <div className="text">
         <h2 id="third section">third section</h2>
