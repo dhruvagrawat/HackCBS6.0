093097d455cc1c49a2648230b9ffac17af0a0d7c
dhruvagrawat <dhruvagrawat9@gmail.com>
2023-11-05 03:55:49 +0530

    new

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
@@ -10,29 +9,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-let nav = document.querySelector("nav");
-window.onscroll = function () {
-  if (document.documentElement.scrollTop > 20) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-}
-
-var typed = new Typed('#element', {
-  strings: ['Tinde..', 'TinDr.'],
-  typeSpeed: 100,
-  backSpeed: 20,
-  smartBackspace: false,
-  loop: true,
-});
-
-
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  });
-}
-
