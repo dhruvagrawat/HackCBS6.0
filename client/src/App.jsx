7f01b5acd7dbe8b600178500b081fb07734d2a03
dhruvagrawat <dhruvagrawat9@gmail.com>
2023-11-05 07:31:29 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
             <a href="#appointment">Appointment</a>
           </li>
           <li>
-            <a href="#">Services</a>
+            <a href="#available">Available slots</a>
           </li>
           <li>
             <a href="#contact">Contact</a>
@@ -86,8 +86,8 @@ function App() {
               </div>
             </div>
             <div className="rightsection">
-              <h1 class="bigtext">Sample text 1</h1>
-              <h6 class="smalltext">sample text 2</h6>
+              <h1 class="bigtext">Tired of booking long Queues</h1>
+              <h6 class="smalltext">Book an appointment </h6>
               {/* content */}
             </div>
           </div>
@@ -124,14 +124,14 @@ function App() {
         {isFormOpen && (
           <form onSubmit={handleSubmit} className="mini-form">
             <label>Location:</label>
-            <select value={location} onChange={handleLocationChange}>
+            <select className ="smolclass" value={location} onChange={handleLocationChange}>
               <option value="Delhi">Delhi</option>
               <option value="Haryana">Haryana</option>
               <option value="UP">UP</option>
             </select>
             <br />
             <label>Hospital Name:</label>
-            <select value={hospital} onChange={handleHospitalChange}>
+            <select className ="smolclass" value={hospital} onChange={handleHospitalChange}>
               <option value="Hospital 1">Hospital 1</option>
               <option value="Hospital 2">Hospital 2</option>
               <option value="Hospital 3">Hospital 3</option>
@@ -145,9 +145,32 @@ function App() {
         )}
       </div>
       <div className="text">
-        <h2 id="third section">third section</h2>
-        <p>Our platform is designed for seamless healthcare scheduling. Users can easily book appointments with top doctors in various hospitals. With a user-friendly interface, scheduling is convenient and straightforward. Our focus is on prioritizing your well-being and time efficiency.</p>
-      </div>
+  <h2 id="available">Available Slots</h2>
+  <p>Find available slots available nearest to you</p>
+  <table className="slot-table">
+    <thead>
+      <tr>
+        <th>Hospital Name</th>
+        <th>Available Slots</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Hospital A</td>
+        <td>10 slots</td>
+      </tr>
+      <tr>
+        <td>Hospital B</td>
+        <td>5 slots</td>
+      </tr>
+      <tr>
+        <td>Hospital C</td>
+        <td>8 slots</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
 
 
       <div className='text'>
@@ -157,24 +180,24 @@ function App() {
     <div className="form-row">
       <div className="form-col">
         <label>First Name:</label>
-        <input type="text" placeholder="Enter your First Name" /> 
+        <input className ="smolclass2" type="text" placeholder="Enter your First Name" /> 
       </div>
       <div className="form-col">
         <label>Last Name:</label>
-        <input type="text" placeholder="Enter your Last Name" />
+        <input className ="smolclass2" type="text" placeholder="Enter your Last Name" />
       </div>
     </div>
     <div className="form-row">
       <label>Email:</label>
-      <input type="email" placeholder="Enter your Email"/>
+      <input className ="smolclass"  type="email" placeholder="Enter your Email"/>
     </div>
     <div className="form-row">
       <label>Phone:</label>
-      <input type="tel" placeholder="Enter your Contact Number"/>
+      <input className ="smolclass"  type="tel" placeholder="Enter your Contact Number"/>
     </div>
     <div className="form-row">
       <label>Message:</label>
-      <textarea placeholder="Enter your message" ></textarea>
+      <textarea className="smolclass1" placeholder="Enter your message" ></textarea>
     </div>
     <button type="submit">Submit</button>
   </form>
